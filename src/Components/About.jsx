a0eca402cd9d60667b1d5a3cf7e5c1560b373a23
Agrima Agrawal <84567933+Agrimaagrawal@users.noreply.github.com>
2023-05-11 21:28:11 +0530

    add

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
                  <h1 className='text-3xl font-bold p-8 pb-0 pt-7 text-white'>I do Designing </h1>
                 <p className='text-xl  p-8 pt-2 text-tgreen'>I design ideas using canva and figma</p>
             </div>
-            <div className='mx-3  w-[500px] h-[200px]  border-tgreen border-2  rounded-md 'data-aos="fade-right">
+            <div className='mx-3  w-[500px] h-auto border-tgreen border-2  rounded-md 'data-aos="fade-right">
                  <h1 className='text-3xl font-bold p-8 pb-0 pt-7 text-white'>I study Computer Science </h1>
                 <p className='text-xl  p-8 pt-2 text-tgreen'>I am BTech 3rd year student who studies Computer Science fundamentals</p>
             </div>
